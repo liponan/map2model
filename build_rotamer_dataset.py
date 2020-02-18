16468edcb759efbec4da85e42499c4b69d9f4bee
leeneil <lishantreasure@gmail.com>
2020-02-18 11:10:53 -0800

    write error log to file

--- build_rotamer_dataset.py
+++ build_rotamer_dataset.py
@@ -7,6 +7,7 @@ def parse_args():
     p = argparse.ArgumentParser(description=__doc__)
     p.add_argument("h5_path", type=str, help="Where to find map h5 files")
     p.add_argument("output_path", type=str, default="./", help="Where to put parsed h5 files")
+    p.add_argument("--error_log", type=str, default=None, help="Log h5 files that coulnd't be processed")
     return p.parse_args()
 
 
@@ -16,13 +17,17 @@ def main():
     h5_files = list_files(args.h5_path)
     for h5_file in h5_files:
         pdb_id = parse_pdb_id(h5_file)
-        print("{} => {}".format(h5_file, pdb_id))
+        dest_path = os.path.join(args.output_path, "{}.h5".format(pdb_id))
+        print("({}) {} => {}".format(pdb_id, h5_file, dest_path))
         try:
             df = parse_h5_attrs(h5_file)
-            df.to_csv(os.path.join(args.output_path, "{}.h5".format(pdb_id)))
+            df.to_csv()
         except:
             print("cannot process {}".format(h5_file))
-
+            if args.error_log is not None:
+                with open(args.error_log, "a") as f:
+                    f.write("{}, {}\n".format(pdb_id, h5_file))
+                    
 
 if __name__ == "__main__":
     main()
